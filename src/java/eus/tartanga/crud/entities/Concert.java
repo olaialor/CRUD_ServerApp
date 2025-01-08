4ffebc4880f5cb0933c39fcc0d057e2a00f2511c
iratisimon <iratisimonuranga@gmail.com>
2025-01-08 11:47:31 +0100

    Model changed

--- src/java/eus/tartanga/crud/entities/Concert.java
+++ src/java/eus/tartanga/crud/entities/Concert.java
@@ -21,34 +21,30 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlTransient;
 
 /**
  *
  * @author Irati
  */
-@Entity
-@Table(name = "concert", schema = "Fanetix")
 
 @NamedQueries({
         @NamedQuery(
             name = "ConcertComingSoon",
-            query = "SELECT c FROM Concert c WHERE c.concertDate >= CURRENT_DATE ORDER BY c.concertDate ASC" //current date -- coming soon
+            query = "SELECT c FROM Concert c WHERE c.concertDate >= CURRENT_DATE ORDER BY c.concertDate ASC" 
     )
     ,
         @NamedQuery(
             name = "ConcertFindBySearchTerm",
             query = "SELECT c FROM Concert c WHERE LOWER(c.concertName) LIKE LOWER(CONCAT('%', :searchTerm, '%')) OR LOWER(c.city) LIKE LOWER(CONCAT('%', :searchTerm, '%')) OR LOWER(c.location) LIKE LOWER(CONCAT('%', :searchTerm, '%'))"
-            //like ---mientra escribes
     ),
         @NamedQuery(
             name = "ConcertFindBetweenDates",
-            query = "SELECT c FROM Concert c WHERE c.concertDate BETWEEN :startDate AND :endDate ORDER BY c.concertDate ASC" //between 2 dates
-            //native query , criteria query , stored procedure ??
+            query = "SELECT c FROM Concert c WHERE c.concertDate BETWEEN :startDate AND :endDate ORDER BY c.concertDate ASC"
     )
 
 })
-
+@Entity
+@Table(name = "concert", schema = "Fanetix")
 @XmlRootElement
 public class Concert implements Serializable {
 
